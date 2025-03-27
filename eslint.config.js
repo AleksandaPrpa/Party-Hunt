2925b7a5ab43c91eee80a1f53c2720dbd9eae4c9
Prpa <andar6230@gmail.com>
2025-03-27 21:23:16 +0100

    -Menjanje eslint configuracije -Dodavanje logo-a u razlicitim velicinama i formatima

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,24 @@
 import { defineConfig } from "eslint/config";
-import globals from "globals";
 import js from "@eslint/js";
+import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  { 
-    files: ["**/*.{js,mjs,cjs,jsx}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    rules: {
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
-    }
+export default defineConfig({
+  languageOptions: {
+    globals: globals.browser,
   },
-  pluginReact.configs.flat.recommended,
-]);
+  plugins: {
+    react: pluginReact,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  rules: {
+    "no-unused-vars": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+});

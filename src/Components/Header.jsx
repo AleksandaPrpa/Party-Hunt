6f98bbb99783d603d3728b4780dac81ef7f93a4a
Prpa <andar6230@gmail.com>
2025-04-05 00:14:00 +0200

    Dodat responsivni dizajn na nav baru

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,62 @@
+import { useState } from "react";
 import logo from "../assets/logo.svg";
 import HeaderNavBarButton from "./HeaderNavBarButton";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="w-screen bg-amber-300 h-16 py-4 px-5 flex flex-row align-middle">
-      <div className="w-5/8">
+    <header
+      className={`w-full h-16 px-5 flex items-center justify-between bg-amber-300`}
+    >
+      <div className="flex items-center">
         <img src={logo} alt="logo" className="h-10" />
       </div>
-      <div className="w-3/8 flex flex-row justify-between">
+      <div className="md:hidden ">
+        <button onClick={toggleMenu}>
+          <svg
+            className="w-8 h-8 text-indigo-900  cursor-pointer"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            {isOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            )}
+          </svg>
+        </button>
+      </div>
+      <nav className="hidden md:flex space-x-4">
         <HeaderNavBarButton href="Profile">Profile</HeaderNavBarButton>
         <HeaderNavBarButton href="Find">Find a Party</HeaderNavBarButton>
         <HeaderNavBarButton href="Throw">Throw a party</HeaderNavBarButton>
-      </div>
-    </div>
+      </nav>
+      {isOpen && (
+        <div
+          className={`absolute top-16 left-0 w-full bg-amber-300 flex flex-col items-center space-y-4 py-4 md:hidden z-50 transition-all duration-300`}
+        >
+          <HeaderNavBarButton href="Profile">Profile</HeaderNavBarButton>
+          <HeaderNavBarButton href="Find">Find a Party</HeaderNavBarButton>
+          <HeaderNavBarButton href="Throw">Throw a party</HeaderNavBarButton>
+        </div>
+      )}
+    </header>
   );
 }
 
--- src/Components/HeaderNavBarButton.jsx
+++ src/Components/HeaderNavBarButton.jsx
@@ -2,7 +2,7 @@ function HeaderNavBarButton({ children, href }) {
   return (
     <a
       href={href}
-      className="text-2xl cursor-pointer text-indigo-900 mx-5 w-1/3 font-Lexend sm:text-s"
+      className="text-2xl cursor-pointer hover:bg-yellow-400 hover:scale-130 duration-300 hover:text-indigo-900 text-indigo-800 font-Lexend sm:text-m w-screen md:w-auto px-4 py-2 flex justify-center items-center"
     >
       {children}
     </a>
